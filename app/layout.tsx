163fac4becef7e569fe49afdcf6f5b1ad17729d0
Muhammad Fahreza <yya77591@gmail.com>
2025-11-04 03:33:12 +0700

    add logo in website

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`relative ${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <PrelineScriptWrapper />
       </body>
