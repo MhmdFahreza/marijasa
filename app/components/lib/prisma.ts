d8f91223d8dd578b970994a2d3f2deb420d36834
Muhammad Fahreza <yya77591@gmail.com>
2026-02-09 11:34:15 +0700

    done fix it

--- app/components/lib/prisma.ts
+++ app/components/lib/prisma.ts
@@ -1,5 +1,5 @@
 // app/components/lib/prisma.ts
-import { PrismaClient } from "@/app/generated/prisma";
+import { PrismaClient } from "@/generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
