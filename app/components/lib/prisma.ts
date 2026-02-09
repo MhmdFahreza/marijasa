5c20e0a0711cb6c099d90eae8c0bb9144de0c522
Muhammad Fahreza <yya77591@gmail.com>
2026-02-09 11:31:11 +0700

    cleaning

--- app/components/lib/prisma.ts
+++ app/components/lib/prisma.ts
@@ -1,5 +1,5 @@
 // app/components/lib/prisma.ts
-import { PrismaClient } from "@/prisma/generated/prisma";
+import { PrismaClient } from "@/app/generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
